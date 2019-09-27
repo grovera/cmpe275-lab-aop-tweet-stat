0b55279dd091415c3996b8f5a0fa664b8d76eee7
Animesh Grover <animesh70@gmail.com>
2019-09-27 05:25:57 -0700

    Added initial files

---
+++ src/main/java/edu/sjsu/cmpe275/aop/tweet/App.java
@@ -0,0 +1,31 @@
+package edu.sjsu.cmpe275.aop.tweet;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        /***
+         * Following is a dummy implementation of App to demonstrate bean creation with Application context.
+         * You may make changes to suit your need, but this file is NOT part of the submission.
+         */
+
+    	ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
+        TweetService tweeter = (TweetService) ctx.getBean("tweetService");
+        TweetStatsService stats = (TweetStatsService) ctx.getBean("tweetStatsService");
+
+        try {
+            int msg = tweeter.tweet("alex", "first tweet");
+            tweeter.follow("bob", "alex");
+            tweeter.retweet("bob", msg);
+            tweeter.block("alex", "bob");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Most productive user: " + stats.getMostProductiveUser());
+        System.out.println("Most popular user: " + stats.getMostFollowedUser());
+        System.out.println("Length of the longest tweet: " + stats.getLengthOfLongestTweet());
+        System.out.println("Most popular message: " + stats.getMostPopularMessage());
+        ctx.close();
+    }
+}
