481907683ebb91d13d022f0d74554d705490d136
Animesh Grover <animesh70@gmail.com>
2019-09-27 20:13:29 -0700

    removed override anotations

--- src/main/java/edu/sjsu/cmpe275/aop/tweet/TweetServiceImpl.java
+++ src/main/java/edu/sjsu/cmpe275/aop/tweet/TweetServiceImpl.java
@@ -10,23 +10,19 @@ public class TweetServiceImpl implements TweetService {
      * You can tweak the implementation to suit your need, but this file is NOT part of the submission.
      */
 
-	@Override
     public int tweet(String user, String message) throws IllegalArgumentException, IOException {
     	System.out.printf("User %s tweeted message: %s\n", user, message);
     	return 0;
     }
 
-	@Override
     public void follow(String follower, String followee) throws IOException {
        	System.out.printf("User %s followed user %s \n", follower, followee);
     }
 
-	@Override
 	public void block(String user, String follower) throws IOException {
        	System.out.printf("User %s blocked user %s \n", user, follower);		
 	}
 
-	@Override
 	public int retweet(String user, int messageId)
 			throws AccessControlException, IllegalArgumentException, IOException {
 		// TODO Auto-generated method stub
--- src/main/java/edu/sjsu/cmpe275/aop/tweet/TweetStatsServiceImpl.java
+++ src/main/java/edu/sjsu/cmpe275/aop/tweet/TweetStatsServiceImpl.java
@@ -6,31 +6,26 @@ public class TweetStatsServiceImpl implements TweetStatsService {
      * You are expected to provide an actual implementation based on the requirements.
      */
 
-	@Override
 	public void resetStatsAndSystem() {
 		// TODO Auto-generated method stub
 		
 	}
-    
-	@Override
+
 	public int getLengthOfLongestTweet() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public String getMostFollowedUser() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String getMostPopularMessage() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	@Override
+
 	public String getMostProductiveUser() {
 		// TODO Auto-generated method stub
 		return null;
